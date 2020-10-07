1e06eef76bf9aac6359456233ebf244a1c184f82
Emmanuel Changole <33663667+EmmanuelChangole@users.noreply.github.com>
2020-10-07 20:58:29 +0300

    Api key

--- app/src/main/java/com/echangole/mymovies/utils/Constants.java
+++ app/src/main/java/com/echangole/mymovies/utils/Constants.java
@@ -2,7 +2,7 @@ package com.echangole.mymovies.utils;
 
 public class Constants
 {
-    public static final String API_KEY="be2b84e612f4e7c73a250de22c3ae3f9";//Get your own api key!!! at http://www.omdbapi.com/
+    public static final String API_KEY="";//Get your own api key!!! at http://www.omdbapi.com/
     public static final String BASE_URL="https://api.themoviedb.org";
     public static final String IMG_URL_500X="https://image.tmdb.org/t/p/w500";
     public static final String IMG_URL_ORIGINAL="https://image.tmdb.org/t/p/original";
